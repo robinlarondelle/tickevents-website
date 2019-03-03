d877ee65be94df253a461fdfd1ce9fdcc85a3a1f
Robin La Rondelle <ra.larondelle@student.avans.nl>
2019-03-03 06:48:08 +0100

    And even more components....

--- ticketstore-client/src/app/app-routing.module.ts
+++ ticketstore-client/src/app/app-routing.module.ts
@@ -4,13 +4,24 @@ import { HomeComponent } from './home/home.component';
 import { EventCreateComponent } from './event-create/event-create.component';
 import { EventsComponent } from './events/events.component';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MyEventsComponent } from './profile/my-events/my-events.component';
+import { MyTicketsComponent } from './profile/my-tickets/my-tickets.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: "/home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "events", component: EventsComponent },
   { path: "login", component: LoginComponent },
-  { path: "create-event", component: EventCreateComponent }
+  { path: "create-event", component: EventCreateComponent },
+  { path: "profile", component: ProfileComponent, children: [
+      { path: "my-events", component: MyEventsComponent },
+      { path: "my-tickets", component: MyTicketsComponent }
+    ]
+  },
+  { path: "404", component: PageNotFoundComponent },
+  { path: "*", redirectTo: "/404" }
 ]
 
 @NgModule({
--- ticketstore-client/src/app/app.module.ts
+++ ticketstore-client/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { EventCreateComponent } from './event-create/event-create.component';
 import { AppRoutingModule } from './app-routing.module';
 import { EventsComponent } from './events/events.component';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MyEventsComponent } from './profile/my-events/my-events.component';
+import { MyTicketsComponent } from './profile/my-tickets/my-tickets.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { LoginComponent } from './login/login.component';
     FooterComponent,
     EventCreateComponent,
     EventsComponent,
-    LoginComponent
+    LoginComponent,
+    ProfileComponent,
+    MyEventsComponent,
+    MyTicketsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
