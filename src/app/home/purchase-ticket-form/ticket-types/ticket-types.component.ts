c6bf6c98e17004cae6117a260f870c0aeb53f647
Robin La Rondelle <ra.larondelle@student.avans.nl>
2019-10-14 07:13:09 +0200

    Prevent user to click to next page when no amount selected

--- src/app/home/purchase-ticket-form/ticket-types/ticket-types.component.ts
+++ src/app/home/purchase-ticket-form/ticket-types/ticket-types.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit, OnDestroy} from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormArray } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { EventService } from 'src/app/shared/services/event.service';
-import { Subscription} from 'rxjs';
+import { Subscription } from 'rxjs';
 import { PurchaseTicketService } from 'src/app/shared/services/purchase-ticket.service';
 import { Event } from 'src/app/shared/models/event.model';
+import { TicketTypeAmount } from 'src/app/shared/models/ticket-type-amount.model';
 
 @Component({
   selector: 'app-ticket-types',
   templateUrl: './ticket-types.component.html',
   styleUrls: ['./ticket-types.component.css']
 })
-export class TicketTypesComponent implements OnInit, OnDestroy  {
+export class TicketTypesComponent implements OnInit, OnDestroy {
   ticketTypeForm: FormArray
   purchaseForm$: Subscription
   total: number = 0
@@ -34,7 +35,7 @@ export class TicketTypesComponent implements OnInit, OnDestroy  {
     this.total = this.purchaseTicketService.purchaseTotal.getValue()
 
     //Get the current available ticket types
-    this.purchaseForm$ = this.purchaseTicketService.purchaseForm$.subscribe(form => {     
+    this.purchaseForm$ = this.purchaseTicketService.purchaseForm$.subscribe(form => {
       this.ticketTypeForm = form.get('tickets') as FormArray
 
       //Get current event
@@ -43,9 +44,9 @@ export class TicketTypesComponent implements OnInit, OnDestroy  {
       })
     })
 
- 
+
     //Get notified when the total purchase amount changes, and update the value to the service
-    this.total$ = this.purchaseTicketService.purchaseTotal.subscribe(purchaseTotal => {      
+    this.total$ = this.purchaseTicketService.purchaseTotal.subscribe(purchaseTotal => {
       this.total = purchaseTotal
     })
 
@@ -59,7 +60,7 @@ export class TicketTypesComponent implements OnInit, OnDestroy  {
 
       this.total = subTotal
     });
-    
+
   }
 
 
@@ -70,13 +71,25 @@ export class TicketTypesComponent implements OnInit, OnDestroy  {
 
   reset() {
     this.ticketTypeForm.controls.map(control => {
-      control.patchValue({amount: 0})            
+      control.patchValue({ amount: 0 })
     })
   }
 
 
   next() {
-    this.router.navigate(["customer-details"], { relativeTo: this.route.parent })
+    let allowContinue = false;
+    this.ticketTypeForm.value.map(ticketTypeAmount => {
+      if (ticketTypeAmount.amount > 0) {
+        allowContinue = true
+      }
+    })    
+
+    if (allowContinue) {
+      this.router.navigate(["customer-details"], { relativeTo: this.route.parent })
+    } else {
+      console.log("not allowed");
+      //alert
+    }
   }
 
 
--- src/app/home/purchase-ticket-form/ticket-types/type/type.component.ts
+++ src/app/home/purchase-ticket-form/ticket-types/type/type.component.ts
@@ -11,41 +11,29 @@ import { TicketType } from 'src/app/shared/models/ticket-type.model';
 export class TypeComponent implements OnInit, AfterViewInit {
   @Input() typeForm: FormGroup
   @ViewChild('amountControl') amountControl: ElementRef
-  @Output() manualChange: EventEmitter<any> = new EventEmitter()
-  type: TicketType
 
 
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute
-  ) { }
+  constructor( ) { }
 
 
   ngOnInit() { }
 
+
   ngAfterViewInit(): void {
     //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
     //Add 'implements AfterViewInit' to the class.
-    this.amountControl.nativeElement.onkeydown = function(e) {
-      if(!((e.keyCode > 95 && e.keyCode < 106)
-        || (e.keyCode > 47 && e.keyCode < 58) 
+    this.amountControl.nativeElement.onkeydown = function (e) {
+      if (!((e.keyCode > 95 && e.keyCode < 106)
+        || (e.keyCode > 47 && e.keyCode < 58)
         || e.keyCode == 8)) {
-          return false;
+        return false;
       }
-  }
-    
-  }
-
-
-  click() {
-    this.router.navigate(["customer-details"], { relativeTo: this.route.parent })
+    }
   }
 
 
   decreaseType() {
-    if (this.typeForm.controls.amount.value > 0) {
     this.typeForm.controls.amount.setValue(this.typeForm.controls.amount.value - 1)
-    }
   }
 
 
